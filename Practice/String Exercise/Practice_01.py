88aad63abaf1b68e5c06b25076e8dbdeed5f7348
Akhil Kumar <75962924+AKHILKAP124@users.noreply.github.com>
2023-12-05 13:24:48 +0530

    Exercise

---
+++ Practice/String Exercise/Practice_01.py
@@ -0,0 +1,15 @@
+"""Create 3 variables to store street, city and country, now create address variable to store entire address. 
+Use two ways of creating this variable, one using + operator and the other using f-string. 
+Now Print the address in such a way that the street, city and country prints in a separate line"""
+
+street = input("Enter Street Name :")
+city = input("Enter City Name :")
+Country = input("Enter Country Name :")
+
+add1 = street + ", " + city + ", " + Country + "."
+add2 = f"{street}, {city}, {Country}."
+
+print(" ")
+print("Adress print form 1 is ",add1)
+print(" ")
+print("Adress print form 1 is ",add2)
---
+++ Practice/String Exercise/Practice_02.py
@@ -0,0 +1,10 @@
+"""Create a variable to store the string "Earth revolves around the sun"
+Print "revolves" using slice operator
+Print "sun" using negative index"""
+
+a = "Earth revolves around the sun"
+print("The Main String is :",a)
+
+print("Slice the ""revolves"" from the Main String")
+slice = a[1]
+print(slice)
